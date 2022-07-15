24acc337cf608dc7ef8561f65b3be6b138d3161c
liadk07 <91607695+liad07@users.noreply.github.com>
2022-07-15 04:39:37 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//site to block
+var arr=["https://www.crackapps.online/"];
+for (let i = 0; i < arr.length; i++) {
+    if (window.location.href.indexOf(arr[i])!=-1){
+        var win = window.open("https://liad07.github.io/site-blocker/", "_self");
+        win.close();
+    }
+
+}
