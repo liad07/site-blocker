fda4ab2182c6efb6f0174d800bbbb27868e69263
liadk07 <91607695+liad07@users.noreply.github.com>
2022-07-15 04:40:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //site to block
-var arr=["https://www.crackapps.online/"];
+var arr=["https://www.google.co.il/"];
 for (let i = 0; i < arr.length; i++) {
     if (window.location.href.indexOf(arr[i])!=-1){
         var win = window.open("https://liad07.github.io/site-blocker/", "_self");
