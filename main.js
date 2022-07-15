3d932a26dfffaddf7b983c8bd438605b7921aad8
liadk07 <91607695+liad07@users.noreply.github.com>
2022-07-15 04:43:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 var arr=["https://www.google.co.il/"];
 for (let i = 0; i < arr.length; i++) {
     if (window.location.href.indexOf(arr[i])!=-1){
-        var win = window.open("https://liad07.github.io/site-blocker/", "_self");
-        win.close();
+        window.location.href="https://liad07.github.io/site-blocker/";
+        //var win = window.open("https://liad07.github.io/site-blocker/", "_self");
+        //win.close();
     }
 
 }
